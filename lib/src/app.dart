60a1e821e617ed032f05b61e2dfd7ebfc2e6f7b1
Alex <lyosha85@gmail.com>
2020-04-22 17:47:03 -0700

    Create a todo for the first custom widget

---
+++ lib/src/app.dart
@@ -0,0 +1,8 @@
+// Import flutter helper library
+
+
+// Create  aclass that will be our custom widget
+// This class must extend the 'StatelessWidget' base class
+
+
+// Define a 'build' method that returns the widgets that *this* widget shows
